a559a37a29412f048751827a41ed2acd7dbb43af
Amal_5900x <amal@keystone.llc>
2023-02-16 18:01:08 +0400

    changes to delay() function to match Flask-RQ2 syntax

--- rq/decorators.py
+++ rq/decorators.py
@@ -53,15 +53,21 @@ class job:  # noqa
     def __call__(self, f):
         @wraps(f)
         def delay(*args, **kwargs):
-            if isinstance(self.queue, string_types):
-                queue = self.queue_class(name=self.queue,
-                                         connection=self.connection)
+
+            if kwargs.get('queue') is not None:
+                queue = kwargs.pop('queue')     ## If queue is passed in delay() function, it overrides the decorator queue
+            elif isinstance(self.queue, str):
+                queue = self.queue_class(name=self.queue, connection=self.connection)
             else:
                 queue = self.queue
 
             depends_on = kwargs.pop('depends_on', None)
             job_id = kwargs.pop('job_id', None)
             at_front = kwargs.pop('at_front', False)
+            timeout = kwargs.pop('timeout', self.timeout)
+            meta = kwargs.pop('meta', self.meta)
+            result_ttl = kwargs.pop('result_ttl', self.result_ttl)
+
 
             if not depends_on:
                 depends_on = self.depends_on
@@ -69,10 +75,24 @@ class job:  # noqa
             if not at_front:
                 at_front = self.at_front
 
-            return queue.enqueue_call(f, args=args, kwargs=kwargs,
-                                      timeout=self.timeout, result_ttl=self.result_ttl,
-                                      ttl=self.ttl, depends_on=depends_on, job_id=job_id, at_front=at_front,
-                                      meta=self.meta, description=self.description, failure_ttl=self.failure_ttl,
-                                      retry=self.retry, on_failure=self.on_failure, on_success=self.on_success)
+            return queue.enqueue_call(
+                f,
+                args=args,
+                kwargs=kwargs,
+                timeout=timeout,
+                result_ttl=result_ttl,
+                ttl=self.ttl,
+                depends_on=depends_on,
+                job_id=job_id,
+                at_front=at_front,
+                meta=meta,
+                description=self.description,
+                failure_ttl=self.failure_ttl,
+                retry=self.retry,
+                on_failure=self.on_failure,
+                on_success=self.on_success,
+            )
+
         f.delay = delay
+        f.queue = delay     ## Added for compatibility with code that currently uses Flask-RQ2
         return f
